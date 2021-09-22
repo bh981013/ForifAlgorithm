408673597d2dd93a3f1a2afe0316eb8baf79723b
MountainNine <goosan126@gmail.com>
2021-09-22 15:54:32 +0900

    add 전화번호 목록/main.py

--- 해시/전화번호 목록/구산/main.py
+++ 해시/전화번호 목록/구산/main.py
@@ -1,1 +1,10 @@
-print('hello world!')
+def solution(phone_book):
+    phone_book = sorted(phone_book, key=lambda x: len(x))
+    for i in range(len(phone_book)):
+        for j in range(len(phone_book)):
+            if phone_book[i] != phone_book[j] and phone_book[i].startswith(phone_book[j]):
+                return False
+    return True
+
+phone_book = ["123456789123456789","23456789"]
+print(solution(phone_book))
