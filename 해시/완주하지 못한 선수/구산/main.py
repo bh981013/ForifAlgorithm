63fcbf571c57e54530e8ecb110e0d7e36287e091
MountainNine <goosan126@gmail.com>
2021-09-22 15:12:15 +0900

    add 완주하지 못한 선수/main.py

--- 해시/완주하지 못한 선수/구산/main.py
+++ 해시/완주하지 못한 선수/구산/main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+def solution(participant, completion):
+    for c in completion:
+        participant.remove(c)
+    answer = participant[0]
+    return answer
+
+participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+completion = ["josipa", "filipa", "marina", "nikola"]
+print(solution(participant, completion))
