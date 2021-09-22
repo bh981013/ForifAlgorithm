9effc232ba5d9aba1fc1b1ef58b43a9f5545d274
MountainNine <goosan126@gmail.com>
2021-09-22 15:26:04 +0900

    fix 완주하지 못한 선수/main.py

--- 해시/완주하지 못한 선수/구산/main.py
+++ 해시/완주하지 못한 선수/구산/main.py
@@ -1,8 +1,16 @@
 def solution(participant, completion):
+    hash_participant = {}
+    for p in participant:
+        if p in hash_participant:
+            hash_participant[p] += 1
+        else:
+            hash_participant[p] = 1
     for c in completion:
-        participant.remove(c)
-    answer = participant[0]
-    return answer
+        if hash_participant[c] == 1:
+            del hash_participant[c]
+        else:
+            hash_participant[c] -= 1
+    return list(hash_participant.keys())[0]
 
 participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
 completion = ["josipa", "filipa", "marina", "nikola"]
