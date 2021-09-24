d7ba1814ce497fc589673eaa2a8ca730312a4886
MountainNine <goosan126@gmail.com>
2021-09-24 14:46:17 +0900

    fix 해시/위장/main.py

--- 해시/위장/구산/main.py
+++ 해시/위장/구산/main.py
@@ -1,25 +1,17 @@
-def get_result(clothes, clothes_hidden, end):
-    result = 0
-    if end:
-        return 0
-
-    for i in range(len(clothes)):
-        end = (i == len(clothes) - 1)
-        if clothes[i][1] not in clothes_hidden:
-            clothes_hidden.append(clothes[i][1])
-            result += 1
-            temp = clothes.copy()
-            temp.remove(temp[i])
-            result += get_result(temp, clothes_hidden, end)
-            clothes_hidden.remove(clothes[i][1])
-    return result
-
 
 def solution(clothes):
-    clothes_hidden = []
-    return get_result(clothes, clothes_hidden, False)
+    clothes_hidden = {}
+    for cloth in clothes:
+        if cloth[1] in clothes_hidden.keys():
+            clothes_hidden[cloth[1]] += 1
+        else:
+            clothes_hidden[cloth[1]] = 1
+    answer = 1
+    for i in clothes_hidden.values():
+        answer *= (i+1)
+    return answer - 1
 
-clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
-# clothes = [["sunglass", "face"], ["glasses", "face"], ["blue_tshirt", "shirt"], ["jeans", "pants"],
-#            ["long_coat", "coat"]]
+# clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+clothes = [["sunglass", "face"], ["glasses", "face"], ["blue_tshirt", "shirt"], ["jeans", "pants"],
+           ["long_coat", "coat"]]
 print(solution(clothes))
