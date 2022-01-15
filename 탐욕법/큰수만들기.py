b79565ebbdef571f671e041a101a9a157e4e5637
bh981013 <bh981013@naver.com>
2022-01-16 01:06:23 +0900

    큰수만들기 최종

--- 탐욕법/큰수만들기.py
+++ 탐욕법/큰수만들기.py
@@ -1,27 +1,51 @@
 #k개의 수를 뺄 수 있을때, 앞에서 k+1개중 가장 큰수를 찾아서 그 앞에있는 모든수를 뺌
 #위 작업을 k=0 이 될때까지 반복
-
+import heapq
 
 def solution(number, k):
     answer = []
-    #numberList = list(map(int, list(number)))
-    index = 0
-    maxIndex = 0
-    while(k>0):
-        maxNum = -1
-        if(index + k == len(number)):    #만약 남은 숫자개수랑 k개수랑 같다면, 즉시 종료
-            return "".join(map(str,answer))
-        
-        for i in range(index, index + k+1):    #가장 큰 수를 찾음
-            if maxNum < int(number[i]):
-                maxNum = int(number[i])
-                maxIndex = i
-        answer.append(maxNum)
-        k = k - (maxIndex - index)
-        index = maxIndex + 1
-    for n in (number[maxIndex+1:]):
-        answer.append(int(n))
+    numberList = list(map(int, list(number)))
+    length = len(number)
+    index = -1
+    maxIndex = -1
+    heap = []
+    delRemain = k
+    jump = 0
+    #처음 k+1개를 확인
+    #k+1개를 전부 확인했다면, 가장 큰 값을 answer에 추가하고 maxIndex를 통해 삭제해야 하는 숫자 개수(k_) 감소
+    #(반복)그 다음 숫자를 heap에 추가->[-number, index]형식
+    #pop했는데 index가 maxIndex보다 작다면, 다시 pop함
+    #pop했는데 index가 maxIndex보다 크다면, maxIndex 를 index값으로 설정하고, k값 결정, number을 answer에 추가
+    #k가 0이 되면 더이상 삭제할수 없으므로, 반복 종료
+    for i in range(k+1):
+        heapq.heappush(heap, [-numberList[i], i])
+    while(True):
+        popped = heapq.heappop(heap)
+        poppedNum = -popped[0]
+        poppedIndex = popped[1]
+       # print("poppedNum: ", poppedNum)
+        if poppedIndex <maxIndex:
+           # print("뒤에꺼임")
+            continue
+        else:
+            #print("삭제가능")
+            delRemain = delRemain -(poppedIndex - maxIndex - 1)   #새로운 답과 기존 답 사이의 값들은 삭제한것임
+            #print("남은 삭제개수:", delRemain)
+            maxIndex = poppedIndex
+            #print(poppedNum)
+            answer.append(poppedNum)
+            if(delRemain == length - maxIndex-1): return "".join(map(str,answer))
+            if(delRemain != 0):
+                heapq.heappush(heap,[-numberList[k+1 + jump], k+1+jump])
+                jump += 1
+                #print("heap에", numberList[maxIndex+1], "추가")
+            else:
+                break
+    for n in numberList[maxIndex+1:]:
+        answer.append(n)
     answer = "".join(map(str,answer))
     return answer
-string = "1924"
-print(solution(string , 2))
+
+
+print(solution("10" , 1))
+
